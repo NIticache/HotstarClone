83ebca9d257a8cefc4a48ccbe4da82455c3165b9
Niteesh <niteeshvijayakumar500@gmail.com>
2022-12-07 15:05:53 +0900

    Completed

--- src/App.js
+++ src/App.js
@@ -4,19 +4,18 @@ import { BrowserRouter as Router ,Routes,Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import Details from './Components/Details';
-import Niti from './Components/Niti';
 function App() {
   return (
     <div className="App">
       <Router>
-        <Niti/>
-        {/* <Header/> */}
-        {/* <Routes>
+        
+        <Header/> 
+         <Routes>
           <Route path='' element={<Login/>}></Route>
           <Route path='home' element={<Home/>}></Route>
           <Route path='detail/:id' element={<Details/>}></Route>
 
-        </Routes> */}
+        </Routes>
       </Router>
     </div>
   );
